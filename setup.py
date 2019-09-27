ee16d37010ec4f1556371b680d90a4ce4458ba85
troido <troido@protonmail.com>
2019-09-27 14:32:57 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ratuil",
-	version="0.0.1",
+	version="0.0.2",
 	author="troido",
 	author_email="troido@protonmail.com",
 	description="A terminal UI library for games",
